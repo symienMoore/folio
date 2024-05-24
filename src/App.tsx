d1e8f0bfdf0d16924dce4ebcef17ce37df1c0e93
symi <symien.moore@gmail.com>
2024-05-24 16:06:43 -0400

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,11 @@ function App() {
   return (
     <div>
       <section className='h-screen'>
-        <div className='mx-auto'>
+        <div className='text-left m-80'>
           <h1 className='font-display font-bold text-white'>
             Symien Moore designs
           </h1>
-          <h1 className='font-display text-white text-8xl font-bold w-20'>
+          <h1 className='font-display text-white text-8xl font-bold w-30'>
             logos, apps & websites.
           </h1>
         </div>
