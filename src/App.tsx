29a4c0e68188af2770992310dd9ec4f3eed4e405
symi <symien.moore@gmail.com>
2024-10-17 23:49:21 -0400

    UI updates

--- src/App.tsx
+++ src/App.tsx
@@ -40,11 +40,11 @@ function App() {
     <div className='container mx-auto'>
       <div className='mt-24 flex flex-col'>
         <motion.h1
-         className='font-bhs lg:text-9xl font-bold text-regal-blue'>
+         className='font-bhs lg:text-9xl font-bold text-regal-blue text-6xl text-center'>
           Symien Moore
           </motion.h1>
           <motion.p
-          className=' lg:w-[600px] text-gray mt-[12px] text-md lg:text-2xl'>
+          className='lg:w-[600px] text-gray mt-[12px] text-2xl text-center'>
             I am a results driven, passionate, designer & developer, 
             focused on the people side of technology. I am a creative
             problem solver, with a passion for creating beautiful,
@@ -56,20 +56,18 @@ function App() {
       <img className='h-[900px]' src="src/assets/profilepic.svg" alt="" />
       <img className='h-[900px]' src="src/assets/profile2.jpeg" alt="" />
       </div> */}
-      <div className='w-full text-regal-blue h-[500px] flex'>
-          <h1 className='mx-auto font-bhs lg:text-7xl pt-72 lg:items-center lg:justify-center'>"people ignore design <br />that ignores people..."</h1>
-      </div>
-      <div className='lg:flex lg:flex-row lg:gap-20 justify-center items-center flex-col'>
-        <img src="src/assets/profilePic.svg" alt="" width={500}/>
-        <div className='w-96'>
-          <p className='lg:text-2xl text-gray'>
+      <div className='flex flex-row items-center'>
+        <h1 className='mx-auto font-bhs text-5xl flex-1'>"people ignore design <br />that ignores people..."</h1>
+        <div className='flex-1'>
+        <img src="src/assets/profilePic.svg" alt="" width={700}/>
+          <p className='text-2xl text-gray '>
             The quote above is from designer Frank Chimero, and it is one that I base
             all my work on. I believe that design should be user-centered, and that
             the user experience should be at the forefront of every design decision.
             I am fortunate enough to have worked with various companies, designers, & 
             developers who share the same belief.
           </p>
-          <button className='bg-regal-blue text-white p-3 rounded'>Get in touch</button>
+          <Button>Get in touch</Button>
         </div>
       </div>
       <div className='mt-48'>
@@ -81,10 +79,10 @@ function App() {
             duration: 0.2,
             delay: 0.2,
           }}
-           key={index} className='lg:flex items-center mt-44'>
-            <div className='flex-1'>
-              <h1 className='lg:text-5xl font-bold font-bhs text-regal-blue'>{post.title}</h1>
-              <p className='text-[#7d7d7d] text-2xl lg:w-1/2'>{post.metadata['description:']}.</p>
+           key={index} className='lg:flex lg:flex-row flex flex-col items-center mt-44'>
+            <div className='flex-1 pl-5 lg:pl-0'>
+              <h1 className='lg:text-5xl text-4xl font-bold font-bhs text-regal-blue'>{post.title}</h1>
+              <p className='text-[#7d7d7d] text-2xl lg:w-1/2 lg:text-left'>{post.metadata['description:']}.</p>
               <p>In progress: <span className={`font-bold ${post.metadata['in_progress:'] == true ? 'text-regal-blue' : ''}`}>{post.metadata['in_progress:'].toString()}</span></p>
               <Button rightIcon={<ArrowForwardIcon />}>
               <Link to={post.slug}>See more</Link>
